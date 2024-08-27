525fc2740796a00d80acddd1e91ee4f474766f00
fayemimichaeloladimeji <fayemi1995@gmail.com>
2024-08-27 01:51:57 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/components/ProductDetail.jsx",
+      "./src/components/ProductList.jsx",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
